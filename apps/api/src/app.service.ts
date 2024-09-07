5bae6ab852817a01cf1e0872c2a99406e8effa7c
shakilshagu <muhammadshakil1968@gmail.com>
2024-09-07 18:03:53 +0530

    "Fix issue with local package linking" "Update dependencies" "Improve UI layout" "Fix authentication bug" "Refactor code for clarity"

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@skl-autospace/sample-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + add(33, 44);
   }
 }
